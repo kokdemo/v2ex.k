0851579ebb90bebfcb791cd5da1d07a849c466bb
kokdemo <kokdemo@gmail.com>
2015-02-07 21:59:35 +0800

    fix bugs

--- README.md
+++ README.md
@@ -11,6 +11,43 @@ v2ex.k 是一个v2ex网站界面修改器，它有如下的特性：
 1. 全新的界面颜色。
 2. 左侧导航栏。
 3. 快速阅读模式。
+4. 左侧显示通知条数。
+5. 自定义颜色设置。
+
+----------
+##帮助
+
+1. 使用快速阅读只需要点击列表中除了链接之外的空白即可。
+2. 自定义颜色设置具体如下
+
+
+    .k_color_dark{
+        background-color: #3C7300;/*主要导航条颜色*/
+    }
+    
+    .k_color_light{
+        background-color: #5CB000;/*次级导航条颜色*/
+    }
+   
+    .k_color_node{
+        background-color: #B9D998;/*小标签背景颜色*/
+    }
+    
+    #Wrapper.k_color_background{
+        background-color: #E2F1D0;/*背景颜色*/
+    }
+    
+    .k_color_hover:hover{
+        background-color: #2980B9;/*导航条放置变色*/
+    }
+    
+    .k_color_item{
+        background-color: #FFF;/*列表项背景颜色*/
+    }
+    
+    .k_color_choosen{
+        background-color: #eeeeee;/*被选择的列表项的背景颜色*/
+    }
 
 for chrome
 
--- js/v2ex.k.js
+++ js/v2ex.k.js
@@ -3,13 +3,14 @@ $(function () {
     var tabbar = $('#Tabs a');
     var avater = $('#Rightbar .box .cell table tbody tr td')[0].innerHTML;
     var search = $('#Search');
-
-    var newNavbar = "<div id='k_navbar' class='bars'></div><div id='k_tabbar' class='bars'></div>";
+    var notificationText = $('.inner a').text().substr(0,5);
+    var notification = '<a href="http://www.v2ex.com/notifications">'+notificationText+'</a>';
+    var newNavbar = "<div id='k_navbar' class='bars k_color_dark'></div><div id='k_tabbar' class='bars k_color_light'></div>";
 
     $('body').prepend(newNavbar);
 
     $('.bars').css('height',window.screen.height);
-    $('#Wrapper').css('width',document.body.clientWidth-150);
+    $('#Wrapper').css('width',document.body.clientWidth-150).addClass('k_color_background');
 
     $('#k_navbar').append(navbar);
     $('#k_tabbar').append(tabbar);
@@ -17,13 +18,19 @@ $(function () {
 
 
     $($('#k_navbar a')[0]).remove();
-    $('#k_navbar').prepend(avater);
-    $($('#k_navbar a')[2]).attr('href','http://www.v2ex.com/new').text('写新主题');
+    $('#k_navbar').prepend(notification).prepend(avater);
+    $($('#k_navbar a')[3]).attr('href','http://www.v2ex.com/new').text('写新主题');
     $('#k_navbar a img').css('border-radius','50%');
+    $('#k_navbar a,#k_tabbar a').addClass('k_color_hover');
+    $('a.count_livid').addClass('k_color_dark');
+    $('a.node').addClass('k_color_node');
 
-    $('#Main .item,#TopicsNode .cell').click(function(){
+    $('#Main .item,#TopicsNode .cell').addClass('k_color_item').click(function(){
         $($('#Rightbar iframe')).remove();
+        $('#Main .item,#TopicsNode .cell').removeClass('k_color_choosen');
+        $(this).addClass('k_color_choosen');
         var iframeUrl = $(this).find('.item_title a').attr('href');
+
         var iframe = '<iframe frameborder=0 seamless allowtransparency="true" width="100%" scrolling="auto" style="margin-bottom:10px; margin-top:-64px" src="'+iframeUrl+' " height="'+(window.screen.height-10)+'">'+'</iframe>';
         $('#Main').css('width',document.body.clientWidth-690-170);
         $('#Rightbar').css('width',690).css('position','fixed').css('right',0).prepend(iframe);
