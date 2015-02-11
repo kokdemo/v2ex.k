e08ccc571242fa912a9a5b0e56dd074479834d03
kokdemo <kokdemo@gmail.com>
2015-02-11 23:36:52 +0800

    0.1.0 version

--- README.md
+++ README.md
@@ -6,19 +6,35 @@ v2ex.k
 
 ---------
 
-v2ex.k 是一个v2ex网站界面修改器，它有如下的特性：
+v2ex.k 是一个有趣的v2ex网站界面修改器，它有如下的特性：
+
+##特性
 
 1. 全新的界面颜色。
 2. 左侧导航栏。
 3. 快速阅读模式。
 4. 左侧显示通知条数。
 5. 自定义颜色设置。
+6. 全键盘操作模式。
+----------
+
+##更新
+
+2015.2.11: 
+
++ 正式迈入0.1版本，修改原有的iframe模式为ajax模式，读取速度更快。
++ 修改CSS，减少页面空隙，提升页面利用率。
++ 增加全键盘操作，提升了笔记本的阅读效率。
++ 优化了快速阅读区域中的中文阅读效果。
++ 优化左侧导航栏的视觉效果，全部使用icon代替原有文字。
 
 ----------
+
 ##帮助
 
 1. 使用快速阅读只需要点击列表中除了链接之外的空白即可。
-2. 自定义颜色设置具体如下
+2. 右侧的快速阅读区域可以点击进入具体的帖子。
+3. 自定义颜色设置具体如下
 
 <code>
 
@@ -51,10 +67,42 @@ v2ex.k 是一个v2ex网站界面修改器，它有如下的特性：
         background-color: #FFF;/*列表项背景颜色*/
     }
     
+    .k_color_item a:link,.k_color_item a:visited,.k_color_item a:active{
+        color: #3C3B47; /*列表项文字颜色*/
+        text-shadow: none;
+    }
+    
     .k_color_choosen{
         background-color: #eeeeee;/*被选择的列表项的背景颜色*/
     }
+    
+    #k_tabbar a.tab_current:link, #k_tabbar a.tab_current:visited, #k_tabbar a.tab_current:active{
+        background-color: #505770; /*次级导航条被选中颜色*/
+        border-top-left-radius:0;
+        border-bottom-left-radius:0;
+    }
 </code>
-for chrome
+
+4 . 全键盘使用：
+
++ 在没有开启快速阅读模式下：
+    + 方向键上，方向键下，会自动选择帖子列表的第一项，并进入快速阅读模式。
+    + 空格键，会回到顶部。
+    + 方向键左，会回到上一页。
+
++ 在开启了快速阅读模式之后：
+    + 方向键上下，切换上一条/下一条帖子。
+    + 空格键，会回到顶部。
+    + 方向键右，进入这个帖子。
+    
+
+##For chrome
 
 https://chrome.google.com/webstore/detail/v2exk/dnbmbhefokngmkalbdcgjdlgoppfhndn
+
+##For Firefox
+
+感谢caoyue的作品：
+
+https://github.com/caoyue/userjs/blob/master/v2ex.k.user.js
+
--- js/v2ex.k.js
+++ js/v2ex.k.js
@@ -1,42 +1,127 @@
 $(function () {
     var navbar = $('#Top .content a');
-    var tabbar = $('#Tabs a');
     var newNavbar = "<div id='k_navbar' class='bars k_color_dark'></div><div id='k_tabbar' class='bars k_color_light'></div>";
-$('.bars').css('height',window.screen.height);
-    $('#Wrapper').css('width',document.body.clientWidth-150).addClass('k_color_background');
 
+    if ($('#Rightbar .box .cell table tbody tr td').length != 0) {
+        var avater = $('#Rightbar .box .cell table tbody tr td')[0].innerHTML;
+        var notificationText = $('.inner a').text();
+        var notificationStart = notificationText.indexOf('未读提醒');
+        notificationText = notificationText.substr(notificationStart - 4, 2);
+    } else {
+        var avater = '<a href="' + navbar[2] + '" class="top k_color_hover">' + navbar[2].innerHTML + '</a>';
+        var notificationText = '';
+    }
+
+    var notification = '<a href="http://www.v2ex.com/notifications"><i class="fa fa-bell fa-2x"></i>' + notificationText + '</a>';
+
+    var k_navbar =
+            avater + notification +
+            '<a href="/" class="top k_color_hover"><i class="fa fa-home fa-2x"></i></a>' +
+            '<a href="http://www.v2ex.com/new" class="top k_color_hover"><i class="fa fa-pencil-square-o fa-2x"></i></a>' +
+            '<a href="https://workspace.v2ex.com/" target="_blank" class="top k_color_hover"><i class="fa fa-laptop fa-2x"></i></a>' +
+            '<a href="/notes" class="top k_color_hover"><i class="fa fa-book fa-2x"></i></a>' +
+            '<a href="/t" class="top k_color_hover"><i class="fa fa-list-alt fa-2x"></i></a>' +
+            '<a href="/events" class="top k_color_hover"><i class="fa fa-eye fa-2x"></i></a>' +
+            '<a href="/place/117.34.170.126" class="top k_color_hover"><i class="fa fa-map-marker fa-2x"></i></a>' +
+            '<a href="/settings" class="top k_color_hover"><i class="fa fa-cog fa-2x"></i></a>'
+        ;
     $('body').prepend(newNavbar);
-    $('#k_navbar').append(navbar);
-    $('#k_tabbar').append(tabbar);
 
+    $('.bars').css('height', window.screen.height);
+    $('#Wrapper').css('width', document.body.clientWidth - 140).addClass('k_color_background');
 
-    var avater = $('#Rightbar .box .cell table tbody tr td')[0].innerHTML;
-    var search = $('#Search');
-    var notificationText = $('.inner a').text();
-    var notificationStart = notificationText.indexOf('未读提醒');
-    notificationText = notificationText.substr(notificationStart-3,5);
+    $('#k_navbar').append(k_navbar).append($('#Top .content a')[9]);
+    $($('#k_navbar a')[10]).html('<i class="fa fa-sign-out fa-2x"></i>');
+    $('#k_tabbar').append($('#Tabs a'));
 
-    var notification = '<a href="http://www.v2ex.com/notifications">'+notificationText+'</a>';
+    $('#Rightbar').prepend($('#Search'));
 
-    $('#Rightbar').prepend(search);
-
-    $($('#k_navbar a')[0]).remove();
-    $('#k_navbar').prepend(notification).prepend(avater);
-    $($('#k_navbar a')[3]).attr('href','http://www.v2ex.com/new').text('写新主题');
-    $('#k_navbar a img').css('border-radius','50%');
+    $('#k_navbar a img').css('border-radius', '50%');
     $('#k_navbar a,#k_tabbar a').addClass('k_color_hover');
     $('a.count_livid').addClass('k_color_count');
     $('a.node').addClass('k_color_node');
 
-    $('#Main .item,#TopicsNode .cell').addClass('k_color_item').click(function(){
-        $($('#Rightbar iframe')).remove();
+    var fast = {
+        changeCSS : function(){
+            $('#Main').css('width', document.body.clientWidth - 690 - 140).css('height', $(window).height()+10);
+            $('#Rightbar').css('width', 690).css('position', 'fixed').css('right', 0);
+        },
+        keyPress:function(event){
+            var key;
+            key = event.keyCode;
+                //Ie使用event.keyCode获取键盘码
+            var dom = $('.k_color_choosen');
+            if(key == 82){
+                //R刷新
+                if(dom.length!=0){
+                    window.location.reload();
+                }
+            }else if(key == 40){
+                //方向键下
+                if(dom.length==0){
+                    $('#Main .item,#TopicsNode .cell')[0].click()
+                }else{
+                    dom.next().click();
+                }
+            }else if(key == 38){
+                //方向键上
+                if(dom.length==0){
+                    $('#Main .item,#TopicsNode .cell')[0].click()
+                }else{
+                    dom.prev().click();
+                }
+            }else if(key == 39){
+                //方向键右
+                if(dom.length!=0){
+                    $('#k_faster').click();
+                }
+            }else if(key == 37){
+                //方向键左
+                if(dom.length==0){
+                    window.history.go(-1);
+                }
+            }else if(key == 32){
+                //空格
+                if(dom.length!=0){
+                    //.click();
+                    document.getElementById('k_faster').scrollTop=0;
+                }else{
+                    document.body.scrollTop = 0;
+                }
+            }
+        }
+    };
+    document.onkeydown = fast.keyPress;
+    $('#Main .item,#TopicsNode .cell').addClass('k_color_item').click(function () {
+        if ($('#Rightbar #k_faster').length == 0) {
+            $('#Rightbar').prepend('<div id="k_faster" class="box" style="height:' + ($(window).height() - 10) + 'px">' + '</div>')
+        }
         $('#Main .item,#TopicsNode .cell').removeClass('k_color_choosen');
         $(this).addClass('k_color_choosen');
-        var iframeUrl = $(this).find('.item_title a').attr('href');
 
-        var iframe = '<iframe frameborder=0 seamless allowtransparency="true" width="100%" scrolling="auto" style="margin-bottom:10px; margin-top:-64px" src="'+iframeUrl+' " height="'+(window.screen.height-10)+'">'+'</iframe>';
-        $('#Main').css('width',document.body.clientWidth-690-170);
-        $('#Rightbar').css('width',690).css('position','fixed').css('right',0).prepend(iframe);
+        var itemUrl = $(this).find('.item_title a').attr('href');
+        var itemID = itemUrl.substr(3, 6);
+        $.ajax({
+            type: "get",
+            url: 'http://www.v2ex.com/api/topics/show.json?id=' + itemID,
+            success: function(data){
+                var title = data[0]['title'];
+                var contentDom = data[0]['content_rendered'];
+                var url = data[0]['url'];
+                var faster = '<h2>' + title + '</h2>' + contentDom;
+                fast.changeCSS();
+                $('#k_faster').html(faster).click(function(){
+                    window.location.href = url;
+                });
+            },
+            error: function(){
+                var iframe = '<iframe frameborder=0 seamless allowtransparency="true" width="100%" scrolling="auto" style="margin-bottom:10px; margin-top:-64px" src="'+itemUrl+' " height="'+(window.screen.height-10)+'">'+'</iframe>';
+                fast.changeCSS();
+                $('#k_faster').html(iframe).css('padding',0).click(function(){
+                    window.location.href = url;
+                });
+            }
+        });
     });
 
 });
