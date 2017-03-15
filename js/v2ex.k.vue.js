a1c3776b04fd5fa5c51246fc8cea8617bd95cce0
kokdemo <kokdemo@gmail.com>
2017-03-15 22:34:00 +0800

    add more themes

--- js/v2ex.k.vue.js
+++ js/v2ex.k.vue.js
@@ -31,42 +31,49 @@ var k_navbar = Vue.extend({
         if (route.login) {
             return ({
                 navbarItems: [{
-                    navbarText: '首页',
-                    navbarUrl: '/',
-                    navbarIcon: 'fa fa-home'
-                }, {
-                    navbarText: '提醒',
-                    navbarUrl: '/notifications',
-                    navbarIcon: 'fa fa-bell'
-                }, {
-                    navbarText: '新主题',
-                    navbarUrl: '/new',
-                    navbarIcon: 'fa fa-pencil-square-o'
-                }, {
-                    navbarText: '奖励',
-                    navbarUrl: '/mission/daily',
-                    navbarIcon: 'fa fa-money'
-                }, {
-                    navbarText: '节点',
-                    navbarUrl: '/planes',
-                    navbarIcon: 'fa fa-th'
-                }, {
-                    navbarText: '笔记',
-                    navbarUrl: '/notes',
-                    navbarIcon: 'fa fa-book'
-                }, {
-                    navbarText: '时间轴',
-                    navbarUrl: '/t',
-                    navbarIcon: 'fa fa-list-alt'
-                }, {
-                    navbarText: '设置',
-                    navbarUrl: '/settings',
-                    navbarIcon: 'fa fa-cog'
-                }, {
-                    navbarText: '退出',
-                    navbarUrl: '/signout',
-                    navbarIcon: 'fa fa-sign-out'
-                }]
+                        navbarText: '首页',
+                        navbarUrl: '/',
+                        navbarIcon: 'fa fa-home'
+                    }, {
+                        navbarText: '提醒',
+                        navbarUrl: '/notifications',
+                        navbarIcon: 'fa fa-bell'
+                    }, {
+                        navbarText: '新主题',
+                        navbarUrl: '/new',
+                        navbarIcon: 'fa fa-pencil-square-o'
+                    }, {
+                        navbarText: '搜索',
+                        navbarUrl: 'https://www.google.com.hk/search?q=site:v2ex.com/t',
+                        navbarIcon: 'fa fa-search'
+                    },
+                    {
+                        navbarText: '奖励',
+                        navbarUrl: '/mission/daily',
+                        navbarIcon: 'fa fa-money'
+                    }, {
+                        navbarText: '节点',
+                        navbarUrl: '/planes',
+                        navbarIcon: 'fa fa-th'
+                    }, {
+                        navbarText: '笔记',
+                        navbarUrl: '/notes',
+                        navbarIcon: 'fa fa-book'
+                    }, {
+                        navbarText: '时间轴',
+                        navbarUrl: '/t',
+                        navbarIcon: 'fa fa-list-alt'
+                    }, {
+                        navbarText: '设置',
+                        navbarUrl: '/settings',
+                        navbarIcon: 'fa fa-cog'
+                    },
+                    {
+                        navbarText: '退出',
+                        navbarUrl: '/signout',
+                        navbarIcon: 'fa fa-sign-out'
+                    }
+                ]
             })
         } else {
             return ({
@@ -91,9 +98,52 @@ var k_navbar = Vue.extend({
         'k_navbar_item': {
             props: ['info'],
             template: '\
-            <div class="k-navbar-item k-hover-nav" v-href="info.navbarUrl">\
+            <div class="k-navbar-item k-nav-hover" v-href="info.navbarUrl">\
                 <i :class="info.navbarIcon"></i>\
                 <span>{{info.navbarText}}</span>\
+            </div>'
+        },
+        'k_navbar_config': {
+            data: function () {
+                return ({
+                    info: {
+                        navbarText: '主题',
+                        navbarIcon: 'fa fa-paint-brush'
+                    },
+                    state: false,
+                    themes: [{
+                        name: '默认主题',
+                        icon: 'fa fa-circle-o-notch',
+                        theme: 'origin'
+                    }, {
+                        name: '亮主题',
+                        icon: 'fa fa-sun-o',
+                        theme: 'light'
+                    }, {
+                        name: '暗主题',
+                        icon: 'fa fa-moon-o',
+                        theme: 'dark'
+                    }]
+                })
+            },
+            methods: {
+                model: function () {
+                    this.state = !this.state;
+                },
+                switchTheme: function (name) {
+                    var userInfo = JSON.parse(localStorage['v2ex.k']);
+                    userInfo['theme'] = name;
+                    localStorage['v2ex.k'] = JSON.stringify(userInfo);
+                    location.reload()
+                }
+            },
+            template: '\
+            <div class="k-navbar-item k-nav-hover" v-on:click="model()">\
+                <i :class="info.navbarIcon"></i>\
+                <span>{{info.navbarText}}</span>\
+            </div>\
+            <div class="k-config" v-show="state">\
+                <div v-for="item in themes" class="k-config-theme" v-on:click="switchTheme(item.theme)"><i :class="item.icon"></i>{{item.name}}</div>\
             </div>'
         },
         'k_avatar': {
@@ -104,13 +154,17 @@ var k_navbar = Vue.extend({
                     login: route.login
                 })
             },
-            template: '<div v-if="login" class="k-avatar k-hover-nav" v-href="userUrl+user.name"><img :src="user.avatar" /><span>{{user.name}}</span></div>'
+            template: '<div v-if="login" class="k-avatar k-nav-hover" v-href="userUrl+user.name">\
+                <img :src="user.avatar" />\
+                <span>{{user.name}}</span>\
+            </div>'
         }
     },
     template: '\
-    <div class="k-navbar k-bg-nav k-text-nav">\
+    <div class="k-navbar k-nav-bg k-nav-text">\
         <k_avatar :user=data ></k_avatar>\
         <k_navbar_item v-for="item in navbarItems" :info="item"></k_navbar_item>\
+        <k_navbar_config></k_navbar_config>\
     </div>'
 })
 Vue.component('k-navbar', k_navbar)
@@ -181,10 +235,14 @@ var k_tabbar = Vue.extend({
     components: {
         'k_tabbar_item': {
             props: ['info', 'route'],
-            template: '<div class="k-tabbar-item k-hover-tab" v-bind:class="{active: info.tabbar == route.tab}" v-href="info.tabbarUrl"><span>{{info.tabbarText}}</span></div>'
+            template: '<div class="k-tabbar-item k-tab-hover" v-bind:class="{active: info.tabbar == route.tab}" v-href="info.tabbarUrl">\
+                <span>{{info.tabbarText}}</span>\
+            </div>'
         }
     },
-    template: '<div class="k-tabbar k-text-tab k-bg-tab"><k_tabbar_item v-for="item in tabbarItems" :info="item" :route="route"></k_tabbar_item></div>'
+    template: '<div class="k-tabbar k-tab-text k-tab-bg">\
+    <k_tabbar_item v-for="item in tabbarItems" :info="item" :route="route"></k_tabbar_item>\
+    </div>'
 })
 Vue.component('k-tabbar', k_tabbar)
 
@@ -205,13 +263,17 @@ var k_forum = Vue.extend({
         'k_list': {
             props: ['list', 'route'],
             template: '\
-            <ul class="k-list k-border-list">\
+            <ul class="k-list k-list-border">\
                 <li class="k-list-item" v-for="item in list" >\
-                    <div class="k-list-item-title-reply"><span class="k-list-item-title" v-on:click="open(item.url)">{{item.title}}</span>\
-                    <span class="k-list-item-reply k-reply-list">{{item.reply}}</span></div>\
-                    <div class="k-list-item-infos k-info-list"><img class="k-list-item-avatar" :src="item.avatar" v-href="item.userUrl">\
-                    <span class="k-list-item-user" v-href="item.userUrl">{{item.userName}}</span>\
-                    <span class="k-list-item-node" v-href="item.node" v-if="!route.node"> &nbsp·&nbsp {{item.nodeText}}</span></div>\
+                    <div class="k-list-item-title-reply">\
+                        <span class="k-list-item-title k-list-text" v-on:click="open(item.url)">{{item.title}}</span>\
+                        <span class="k-list-item-reply k-list-reply k-reply-list">{{item.reply}}</span>\
+                    </div>\
+                    <div class="k-list-item-infos k-list-text-2">\
+                        <img class="k-list-item-avatar" :src="item.avatar" v-href="item.userUrl">\
+                        <span class="k-list-item-user" v-href="item.userUrl">{{item.userName}}</span>\
+                        <span class="k-list-item-node" v-href="item.node" v-if="!route.node"> &nbsp·&nbsp {{item.nodeText}}</span>\
+                    </div>\
                 </li>\
             </ul>',
             methods: {
@@ -237,19 +299,19 @@ var k_forum = Vue.extend({
                 }
             },
             template: '\
-            <div class="k-page">\
+            <div class="k-page ">\
                 <span v-if="ifLastpage" v-href="lastpage">上一页</span>\
                 <span class="k-page-next" v-href="nextpage">下一页</span>\
             </div>'
         },
         'k_frame': {
             props: ['url'],
-            template: '<iframe :src=url class="k-frame"></iframe>'
+            template: '<iframe :src=url class="k-frame k-iframe-bg"></iframe>'
         }
     },
     template: '\
     <div class="k-forum">\
-        <div class="k-page-list k-bg-list k-text-list"><k_page :route="route"></k_page>\
+        <div class="k-page-list k-list-bg k-list-text"><k_page :route="route"></k_page>\
         <k_list :list="listItems" :route="route"></k_list><k_page :route="route"></k_page></div>\
         <k_frame class="" :url="iframeUrl"></k_frame>\
     </div>'
@@ -329,11 +391,14 @@ var getUserInfo = function (route) {
             userInfo.name = document.getElementById("Rightbar").getElementsByTagName("a")[0].href.split("/member/")[1];
             userInfo.avatar = document.getElementById("Rightbar").getElementsByTagName("img")[0].src;
             userInfo.version = version;
+            userInfo.theme = 'origin'
             localStorage['v2ex.k'] = JSON.stringify(userInfo);
         } else if (localStorage['v2ex.k'].version !== version || localStorage['v2ex.k'].version === version) {
+            // 重置
             userInfo.name = document.getElementById("Rightbar").getElementsByTagName("a")[0].href.split("/member/")[1];
             userInfo.avatar = document.getElementById("Rightbar").getElementsByTagName("img")[0].src;
             userInfo.version = version;
+            userInfo.theme = 'origin'
             localStorage['v2ex.k'] = JSON.stringify(userInfo);
         }
     } else {
@@ -367,6 +432,12 @@ ready(function () {
     };
     // console.info(route);
     var userData = getUserInfo(route);
+    if (userData.theme == 'light') {
+        document.body.className = 'k-light'
+    } else if (userData.theme == 'dark') {
+        document.body.className = 'k-dark'
+    }
+
     if (!route.index && !route.node && !route.recent && !route.mytopics) {
         // 在非首页，recent，node页面中，展示原来的页面
         document.getElementById('Main').style.display = 'block';
@@ -406,10 +477,10 @@ ready(function () {
                     });
                 },
                 template: '<div class="k-container" v-bind:class="{origin:iforigin}" v-if="!route.iframe">\
-                                <k-navbar :data="userData"></k-navbar>\
-                                <k-tabbar></k-tabbar>\
-                                <k-main v-if="route.index||route.node||route.recent||route.mytopics"></k-main>\
-                            </div>'
+                    <k-navbar :data="userData"></k-navbar>\
+                    <k-tabbar></k-tabbar>\
+                    <k-main v-if="route.index||route.node||route.recent||route.mytopics"></k-main>\
+                </div>'
             })
             document.body.appendChild(document.createElement('k-container'));
         }
